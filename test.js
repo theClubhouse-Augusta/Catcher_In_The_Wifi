640115c4aa307cbdfb302ae7542c040dd5071452
Taylor Williams <51328545+Twilliams19@users.noreply.github.com>
2019-08-05 15:09:11 -0400

    HttpServer_version1

---
+++ test.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var url = require('url');
+// var db = require ('./db');
+
+var PORT = 8080;
+
+  http.createServer(function (req, res){
+   
+    var parUrl = url.parse(req.url, true);
+    
+
+    })
+   .listen(8080, function() {
+       console.log("Now listening to Port:" + 8080);
+   
+});
