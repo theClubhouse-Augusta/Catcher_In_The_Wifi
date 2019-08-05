1153fe7550997f5da367592f91f97488779e529d
Taylor Williams <51328545+Twilliams19@users.noreply.github.com>
2019-08-05 15:45:03 -0400

    change file name, removed unneeded variables

---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+
+
+
+
+  http.createServer(function (req, res){
+   
+    
+    
+
+    })
+   .listen(8080, function() {
+       console.log("Now listening to Port:" + 8080);
+   
+});
