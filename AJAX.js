f4b54818cf63455164201e3e4c0f3798e0c717bc
Rain <51328164+rellarain@users.noreply.github.com>
2019-08-06 12:30:06 -0400

    Add files via upload

---
+++ AJAX.js
@@ -0,0 +1,19 @@
+"use strict"
+
+
+var emailRaw = document.getElementById("email").value;
+var eventRaw = document.getElementById("eventSelect").value;
+var email = encodeURIComponent(emailRaw);
+var event = encodeURIComponent(eventRaw);
+function encode() {
+   var xhttp = new XMLHttpRequest();
+   xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+         document.getElementById("eventSelect").value = this.responseText;
+      }
+      var request = "/add?" + "&email=" + email + "&event=" + event;
+      xhttp.open("GET", request, true);
+      xhttp.send();
+   };
+}
+encode();
