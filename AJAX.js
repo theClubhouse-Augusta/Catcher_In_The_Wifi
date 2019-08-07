14a2048e9ddf665c9efbb227d245b18936084dd1
rellarain <aurella@auraprogram.com>
2019-08-07 15:27:21 -0400

    Page Title Update

--- AJAX.js
+++ AJAX.js
@@ -1,8 +1,7 @@
 "use strict"
-//var click = document.getElementById("submit");
+
 
 function encode() {
-//console.log("lala")
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
 if (this.readyState == 4 && this.status == 200) {
@@ -12,21 +11,9 @@ console.log(JSON.parse(res));
 }
 };
 
-//click.addEventListener("click", encode);
-
-//var emailRaw = document.getElementById("email").value;
-//var eventRaw = document.getElementById("eventSelect").value;
 var email = encodeURIComponent(email);
 var event = encodeURIComponent(event);
 var request = "/add?" + "email=" + email + "&event=" + event;
-   //xhttp.onreadystatechange = function() {
    xhttp.open("GET",request, true);
    xhttp.send();
 };
-      
-      //if (this.readyState == 4 && this.status == 200) {
-      
-      //xhttp.open("GET",  request, true);
-      
-   //};
-//}
