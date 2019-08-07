99a6161698723cacb31062741cfdcdb8d8d18c5e
rellarain <aurella@auraprogram.com>
2019-08-07 14:05:46 -0400

    corrections hopefully

--- AJAX.js
+++ AJAX.js
@@ -1,18 +1,32 @@
 "use strict"
+//var click = document.getElementById("submit");
 
-
-var emailRaw = document.getElementById("email");
-var eventRaw = document.getElementById("eventSelect");
-var email = encodeURIComponent(emailRaw);
-var event = encodeURIComponent(eventRaw);
 function encode() {
-   var xhttp = new XMLHttpRequest();
-   xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-         
-      }
-      var request = "/add?" + "&email=" + email + "&event=" + event;
-      xhttp.open("GET", request, true);
-      xhttp.send();
-   };
+//console.log("lala")
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+if (this.readyState == 4 && this.status == 200) {
+
+var res = this.responseText;
+console.log(JSON.parse(res));
 }
+};
+
+//click.addEventListener("click", encode);
+
+//var emailRaw = document.getElementById("email").value;
+//var eventRaw = document.getElementById("eventSelect").value;
+var email = encodeURIComponent(email);
+var event = encodeURIComponent(event);
+var request = "/add?" + "email=" + email + "&event=" + event;
+   //xhttp.onreadystatechange = function() {
+   xhttp.open("GET",request, true);
+   xhttp.send();
+};
+      
+      //if (this.readyState == 4 && this.status == 200) {
+      
+      //xhttp.open("GET",  request, true);
+      
+   //};
+//}
