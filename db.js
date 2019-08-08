aa0826cc1ef69899217431fac527d55d556f610e
vcjronijohn <ronijohnveronica@gmail.com>
2019-08-08 10:53:27 -0400

    to incorporate all code blocks into one file. is this ok?

--- db.js
+++ db.js
@@ -1,13 +1,35 @@
-var mysql = require('mysql');
+var mysql = require('mysql'),
+    config  = require("./config.json"),
+    email = "";
+    
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "Catcher_In_The_Wifi",
-});
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+function getCaughtData(){
+    var connection = mysql.createConnection(config.db);
+    connection.connect(function(err) {
+        if(err) throw err;
+        connection.query("SELECT * FROM caughtInfo", function(err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+            connection.end(); // close the connection
+        })
+    }) 
+}
+
+function insertCaughtData(email) {
+    var connection = mysql.createConnection(config.db);
+    connection.connect(function(err) {
+        if (err) throw err;
+        var sql = "INSERT INTO caughtInfo (email) VALUES ('" + email +"')";
+        connection.query(sql, function(err, result) {
+         if (err) throw err;
+         console.log("Email entered");
+         connection.end(); // close the connection
+     })
+    })
+}
+getCaughtData(); //  test call
+insertCaughtData(email); //test call
+
+module.exports.getCaughtData = getCaughtData;
+module.exports.insertCaughtData = insertCaughtData;
