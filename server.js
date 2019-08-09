1a935162800b9f8de66180d04d5c22a53afb73f7
Melva Squires <melva.a.squires@gmail.com>
2019-08-09 13:01:51 -0400

    file used to serve page in VS live-server

--- server.js
+++ server.js
@@ -1,39 +1,29 @@
-var http = require("http");
+var http = require('http');
+var fs = require('fs');
 var url = require('url');
-var fs = require("fs");
-var path = require('path');
+var db = require('./db');
 
-const server = http.createServer(function (req, res) {
+var server = http.createServer(function(req, res){
+  
+    var parUrl = url.parse(req.url, true);
+    var pathname = '.';
+    res.writeHead(200, {"Content-Type": "application/json", "access-control-allow-origin": "*"});
+    console.log('request was made: ' + req.url);
 
-		var parUrl = url.parse(req.url, true);
-		var filePath = '.' + parUrl.pathname;
-
-
-
-
-// Serving pages
-
-		if(parUrl.pathname === ('/')){
-			fs.readFile("index.html", function (err, content) {
-			res.writeHead(200, { 'Content-Type': "text/html" });
-			return res.end(content);
-		});
-		} else if (parUrl.pathname === ('/index.css')){
-		fs.readFile("index.css", function(err, content){
-			res.writeHead(200, {'content-Type': "text/css"});
-			res.end(content);
-		});
-	    } else if (parUrl.pathname === ('/reset.css')){
-		fs.readFile("reset.css", function(err, content){
-			res.writeHead(200, {'content-Type': "text/css"});
-			res.end(content);
-		});
-	    } 
-		
-})
-
-	server.listen(8080, function () {
-			console.log("Listening on Port: "+ 8080);
-		});
-
-
+    if (req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res); 
+    }    
+    if (req.url === '/style.css'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/style.css').pipe(res); 
+    }
+    if (req.url === '/reset.css'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/reset.css').pipe(res); 
+    }
+  
+});
+  
+server.listen(8080, '127.0.0.1');
+console.log('The server is listening on port ' + 8080);
