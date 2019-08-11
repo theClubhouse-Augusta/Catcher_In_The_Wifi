509bce22da6690543ece85b2a92efcb97c59c985
Feevah <cddefever@gmail.com>
2019-08-11 17:59:38 -0400

    newFeatures

--- src/server.js
+++ src/server.js
@@ -11,13 +11,26 @@ app.use(express.static(publicDirectory))
 
 
 app.get('/add', (req, res) => {
-	console.log(req.query);
 	var emailMe =  req.query.email;
     var event = req.query.event;
-    console.log(emailMe);
-    db.insertCaughtData(emailMe);
-    res.redirect();
+
+    // I only want to insert email if value is not null
+
+    if (emailMe !== null){
+       db.insertCaughtData(emailMe)
+    };
+
+    // After inserting data, I want to redirect to empty input box 
+    res.redirect('');
 })
+
+app.get('/snow/dog', (req, res) => {
+
+ 	// We can console log data, but I want to render database at url /snow/dog
+		console.log(db.getCaughtData());
+})
+
+	// Still need 404
 
 
 
